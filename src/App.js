c6eae86e2bfbdfe7653705b21a531a706d3ce836
Arkadiusz <arkadiusz@ostrowski.co>
2020-01-30 09:22:26 +0000

    add react router to website, rename pages.js to photos.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './styles.css'
 import Header from "./components/Header";
+import {Switch, Route} from 'react-router-dom'
+import Cart from "./pages/Cart";
+import Photos from "./pages/Photos";
 
 function App() {
   return (
     <div>
       <Header />
-      <h1>Home Page</h1>
+
+      <Switch>
+        <Route exact path='/'>
+          <Photos />
+        </Route>
+        <Route path='/cart'>
+          <Cart/>
+        </Route>
+      </Switch>
     </div>
   );
 }
