18ef2ee7082f4813adc8f6af3e297609a5709586
Arkadiusz <arkadiusz@ostrowski.co>
2020-01-30 12:02:54 +0000

    add hear and cart icon, render on hover

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 
 // TODO update .json file with alt property for each image, add few more photos
 
 export default function Image({className, img}) {
+  const [isHovered, setIsHovered] = useState(false);
 
-  const imageClassName = typeof className === 'undefined' ? '' : className;
+  const imageSize = typeof className === 'undefined' ? '' : className;
+
+  const heartIcon = isHovered && <i className="ri-heart-line favorite"/>;
+  const cartIcon = isHovered && <i className="ri-add-circle-line cart"/>;
 
   return(
-    <div className={`${imageClassName} image-container`}>
-      <img alt='' src={img.url} className="image-grid" />
+    <div
+      className={`${imageSize} image-container`}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <img
+        alt=''
+        src={img.url}
+        className="image-grid"
+      />
+      {heartIcon}
+      {cartIcon}
     </div>
   )
 }
