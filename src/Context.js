d98df6a9129cd3546579aee94ea21c1a5b648dea
Arkadiusz <arkadiusz@ostrowski.co>
2020-01-30 10:50:26 +0000

    fetch data from github repo with images and save it in context state

--- src/Context.js
+++ src/Context.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 const Context = React.createContext(undefined, undefined);
 
 function ContextProvider(props) {
+  const [allPhotos, setAllPhotos] = useState([]);
+
+  const url = 'https://raw.githubusercontent.com/akdsco/memory-prints-images/master/images.json';
+
+  useEffect(() => {
+    fetch(url)
+      .then(res => res.json())
+      .then(data => setAllPhotos(data));
+  },[]);
+
   return(
-    <Context.Provider value=''>
+    <Context.Provider value={{allPhotos}}>
       {props.children}
     </Context.Provider>
   )
