230d7775900d4512b56d280a4631249128d1ec76
Arkadiusz <arkadiusz@ostrowski.co>
2020-02-01 10:44:28 +0000

    change name and colour palette

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ function Header() {
   return(
     <header>
       <Link to='/'>
-        <h2>Pic Some</h2>
+        <h2>minimal prints</h2>
       </Link>
       <Link to='/cart'>
           <i className={`${cartClassName} ri-fw ri-2x`}/>
